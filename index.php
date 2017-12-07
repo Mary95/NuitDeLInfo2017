b0d7a734b6ab0d4bf33d4de233befe2665890951
Mary95 <mariemouret@free.fr>
2017-12-07 21:36:01 +0100

    création d'un menu permettant de modifier le contenu du corps de la page sans changer de page

--- index.php
+++ index.php
@@ -6,10 +6,27 @@
 		<title> Accueil </title>
 	</head>
 	<body>
-	hello word
-	<?php
-		echo("hello word 2");
-	?>
+		<div id="menu">
+			<a href="#?id=01"> revoir les explications </a>
+			<a href="#?id=02"> acceder au jeu </a>
+			<a href="#?id=03"> à propos du projet </a>
+		</div>
+		<?php
+			if(isset($_GET['id'])!=null)
+				{
+					$page = $_GET['id'];
+				}
+				else
+				{
+					$page = 0;
+				}
+			echo $page;
+			$pageAccueil = 
+			$pageExplication = 
+			$pageJeux = '<div id="question">Affichage des questions  </div> <div id="jeux"> affichage du jeux</div>';
+			$pageAbout = '<div> à propos du projet</div>';
+		
+		?>
 	</body>
 
 </html>
