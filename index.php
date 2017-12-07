f417c562bb599ecacdb7451b0fb87f617f0c4aa0
Mary95 <mariemouret@free.fr>
2017-12-07 21:46:30 +0100

    rechargement de la page

--- index.php
+++ index.php
@@ -3,14 +3,16 @@
 <html>
 
 	<head>
+		<script src="rechargement.js"> </script>
 		<title> Accueil </title>
 	</head>
 	<body>
 		<div id="menu">
-			<a href="#?id=01"> revoir les explications </a>
-			<a href="#?id=02"> acceder au jeu </a>
-			<a href="#?id=03"> à propos du projet </a>
+			<a href="#?id=01" onclick= "refresh()"> revoir les explications </a>
+			<a href="#?id=02" onclick= "refresh()"> acceder au jeu </a>
+			<a href="#?id=03" onclick= "refresh()"> à propos du projet </a>
 		</div>
+		<div id="corps">
 		<?php
 			if(isset($_GET['id'])!=null)
 				{
@@ -20,13 +22,15 @@
 				{
 					$page = 0;
 				}
+				
 			echo $page;
-			$pageAccueil = 
-			$pageExplication = 
-			$pageJeux = '<div id="question">Affichage des questions  </div> <div id="jeux"> affichage du jeux</div>';
-			$pageAbout = '<div> à propos du projet</div>';
-		
-		?>
+		//		$pageAccueil = 
+			//	$pageExplication = 
+				//$pageJeux = '<div id="question">Affichage des questions  </div> <div id="jeux"> affichage du jeux</div>';
+				//$pageAbout = '<div> à propos du projet</div>';
+			
+			?>
+		</div>
 	</body>
 
 </html>
